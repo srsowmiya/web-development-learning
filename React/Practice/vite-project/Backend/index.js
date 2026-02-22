8ee7c73fefa935a5ddac9d40095ecd5882cbb59d
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-02-22 21:08:13 +0530

    sign up logic

--- React/Practice/vite-project/Backend/index.js
+++ React/Practice/vite-project/Backend/index.js
@@ -1,10 +1,17 @@
 const express= require('express')
 const user=require('./routes/userRoutes')
+const cors=require('cors')
+const mongoose=require('mongoose')
 
 const app=express()
+app.use(cors())
+app.use(express.json())
 
 app.use('/signup',user)
-app.use(express.json())
+
+
+
+mongoose.connect("mongodb://localhost:27017/Authentication-task")
 
 app.listen(5000,()=>{
     console.log("started server on the port 5000")
--- React/Practice/vite-project/Backend/routes/userRoutes.js
+++ React/Practice/vite-project/Backend/routes/userRoutes.js
@@ -1,10 +1,22 @@
 const express=require('express')
-const userSchema=require('../modal/user')
+const userSchema=require('../model/user')
 
 const router=express.Router()
 
-router.post('/signup',(req,res)=>{
-    res.send({msg:"reached the post home request"})
+router.post('/signup',async(req,res)=>{
+    try{
+        const {name,email,password}=req.body
+        const newUser= new userSchema({
+            name,
+            email,
+            password
+    })
+    await newUser.save()
+    res.status(201).json({message:"registration successfull"})
+    }
+    catch(error){
+        res.status(400).json({message:"registration failed",error:error.message})
+    }
 })
 
 module.exports=router
