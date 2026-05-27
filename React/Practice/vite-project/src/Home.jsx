458e71be1e636596c54ccb874991dae26c2a05ac
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-05-27 11:23:51 +0530

    Practiced useEffect

--- React/Practice/vite-project/src/Home.jsx
+++ React/Practice/vite-project/src/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {useEffect} from 'react'
 
 const Home=()=>{
+    const arr=["sowmiya","sharvesh","sobhika"]
+    useEffect(()=>{
+        console.log("Home component rendered")
+    },[])
     return(
         <>
+        <ul>{arr.map(val=>(<li>{val}</li>))}</ul>
         <h1>Welcome to the home page</h1>
         </>
     )
