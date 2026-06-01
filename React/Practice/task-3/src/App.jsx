0f07cc9849ce1ce4d9791bebad85967db8d97737
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-01 10:20:51 +0530

    digital clock created using useEffect

--- React/Practice/task-3/src/App.jsx
+++ React/Practice/task-3/src/App.jsx
@@ -5,7 +5,7 @@ const App=()=>{
   return(
     <div style={{backgroundColor:color,minHeight: '100vh',  
       width: '100%'}}>
-    <h1>Welcome to colour changing backgroun</h1>
+    <h1>Welcome to colour changing background</h1>
     <button value="green" onClick={(e)=>setColor(e.target.value)} >Green</button>
     <button value="red" onClick={(e)=>setColor(e.target.value)}>Red</button>
     <button value="blue" onClick={(e)=>setColor(e.target.value)}>Blue</button>
