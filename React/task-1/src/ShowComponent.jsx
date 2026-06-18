432fcf5761db3f81e7021975353b864a1659e93b
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-18 12:10:16 +0530

    small task in react

---
+++ React/task-1/src/ShowComponent.jsx
@@ -0,0 +1,11 @@
+const ShowComponent=()=>{
+    const isTrue=true
+        return(
+
+        <>
+       {isTrue && <h1>Show Component</h1>}
+        </>
+        )
+}
+
+export default ShowComponent
--- React/task-1/src/main.jsx
+++ React/task-1/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Notes from './Notes.jsx'
+import ShowComponent from './ShowComponent.jsx'
 import Header from './components/Header.jsx'
 import Signin from './Signin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Notes/>
+    <ShowComponent/>
   </StrictMode>,
 )
