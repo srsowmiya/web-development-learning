9404440c009a963ab9a4125a572af2648bb2813d
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-18 11:56:51 +0530

    lists and keys

--- React/task-1/src/Notes.jsx
+++ React/task-1/src/Notes.jsx
@@ -1,17 +1,14 @@
 import {useState} from 'react'
 const Notes =()=>{
-    const [note,setNote]=useState('')
-    function handleSubmit(e){
-        setNote(e.target.value)
-        
-    }
-    return(
-        <>
-        <h1>Notes</h1>
-        <textarea onChange={handleSubmit}></textarea>
-        <button >Add Notes</button>
-        </>
-    )
+    const arr=["sowmiya","sharvesh","sobhika","divya"]
+   return(
+    <>
+    <h1>Students</h1>
+    <ul>
+        {arr.map((items,key)=><li key={key}>{items}</li>)}
+    </ul>
+    </>
+   )
 }
 
 export default Notes
