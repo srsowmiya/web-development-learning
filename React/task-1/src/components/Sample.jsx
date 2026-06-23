3c37136e0938619ba84fa832fcb4b83c0458dde2
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-23 10:49:09 +0530

    useEffect

--- React/task-1/src/components/Sample.jsx
+++ React/task-1/src/components/Sample.jsx
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useRef,useEffect } from 'react';
 
 const Sample = () => {
   const val = useRef(0);
+  useEffect(()=>{
+    console.log("useEffect called")
+  },[])
 
   function increase() {
     val.current++;
