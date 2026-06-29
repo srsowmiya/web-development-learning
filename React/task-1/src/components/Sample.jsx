6cca1200aa32cd7e7a2b6fb673ebc618251bb263
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-29 15:31:34 +0530

    js date object

--- React/task-1/src/components/Sample.jsx
+++ React/task-1/src/components/Sample.jsx
@@ -1,22 +1,13 @@
-import { useRef,useEffect } from 'react';
+import React from 'react'
 
+const date=new Date()
 const Sample = () => {
-  const val = useRef(0);
-  useEffect(()=>{
-    console.log("useEffect called")
-  },[])
-
-  function increase() {
-    val.current++;
-    console.log(val.current);
-  }
-
   return (
     <div>
       <h1>Hello World</h1>
-      <button onClick={increase}>Increase</button>
+      <h1>{date.getDay()}</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Sample;
+export default Sample
