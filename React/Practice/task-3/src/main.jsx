e0eaa43d7ffe3d2c7184070244a56b086dcf79a4
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-07-11 14:36:12 +0530

    code organised

--- React/Practice/task-3/src/main.jsx
+++ React/Practice/task-3/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
 import State from './State'
+import Profiles from './Profiles'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <State />
+    <Profiles />
   </StrictMode>,
 )
