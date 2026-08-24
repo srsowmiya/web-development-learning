de873fa4b1fdfc2704741502f07600463934e903
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-08-24 16:23:58 +0530

    added code generation

--- React/Secret-message/Frontend/src/App.jsx
+++ React/Secret-message/Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 const App = () => {
   const [code,setCode]=useState('')
+  const [message,setMessage]=useState('')
 
   async function handleSubmit(){
     const resp=await fetch('http://localhost:5000/code')
@@ -13,12 +14,22 @@ const App = () => {
       alert("error generating code")
   }
 
+  async function handleMessage(){
+    const res=await fetch('http://localhost:5000/message')
+    if(res.ok){
+      const data=await res.json()
+      setMessage(data.result)
+    }
+  }
+
   return (
     <div>
         <h1>Send Secret Messages</h1>      
         <button onClick={handleSubmit}>Generate Code</button>
         <h2>{code}</h2>
-        <input type='text'/>
+        <input type='text' onChange={(e)=>{setMessage(e.target.value)}}/>
+        <button onClick={handleMessage}></button>
+        <h3>{message}</h3>
     </div>
   )
 }
